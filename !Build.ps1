f9eed48d650ca79a08feabae1e169ddf36cf5949
Chrysaloid <58228915+Chrysaloid@users.noreply.github.com>
2025-11-01 15:12:31 +0100

    Fixed the build script

---
+++ !Build.ps1
@@ -0,0 +1,18 @@
+﻿tsc --build --verbose
+
+vsce package --allow-missing-repository --allow-star-activation
+Write-Host
+
+$name = Get-ChildItem -Name -File -Filter *.vsix | Select-Object -First 1
+Invoke-Expression "code --force --install-extension $name"
+Write-Host
+
+Write-Host "AHK"
+RunAhk.ps1 @'
+WinActivate "ahk_exe Code.exe"
+WinWaitActive "ahk_exe Code.exe"
+Send "^!+r"
+'@
+Write-Host
+
+PressEnterToContinue.ps1
